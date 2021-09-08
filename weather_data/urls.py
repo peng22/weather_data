08d27f8b0d9b14fda3056e5b2f7698dc70c020dd
peng22 <mohamed_mido22107@yahoo.com>
2021-09-08 09:47:40 +0200

    fixing urls navigate the home url to the add weather data

--- weather_data/urls.py
+++ weather_data/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('add_weather_data',views.add_weather_data,name='add_weather_data'),
+    path('',views.add_weather_data,name='add_weather_data'),
     path('list_weather_data',views.list_weather_data,name='list_weather_data'),
     path('summary_data',views.summary_data,name='summary_data')
 ]
